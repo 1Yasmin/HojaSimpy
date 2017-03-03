51865fe89e1950ac816dddfbf825c97f48fb72fa
Yasmin Chavez <Yasmin Chavez>
2017-03-03 11:53:41 -0600

    Final

--- SimuladorProcesos.py
+++ SimuladorProcesos.py
@@ -7,15 +7,18 @@ import random
 import simpy
 import time
 
-t0 = time.clock()
+#t0 = time.clock()
 
 def esteProceso (nombre, env, CPU, espacioRAM):
+    global totalDia
     
     velocidadProc = 1
-    numInstrucc = 3
+    numInstrucc = 6
     valida = True
-    instruccPorRealizar = random.expovariate(0.1/10)
+    instruccPorRealizar = random.expovariate(0.1/1)
     memoriaActual = random.randint(1,10)
+    
+    
     while (valida):
         if capacidadMemoria.level >= memoriaActual:
             yield capacidadMemoria.get(memoriaActual)
@@ -23,38 +26,44 @@ def esteProceso (nombre, env, CPU, espacioRAM):
             valida = False
         else:
             print('%s esta esperando RAM' % nombre)
-            yield env.timeout(1)
+            valida = False
             
     #Verificar que hayan procesos faltantes
-    while (instruccPorRealizar > 1):
-        print ('%s ya esta ready' % nombre)
-        with CPU.request() as turno:
-                yield turno
-                print ('%s ya entro al procesador' % nombre)
-                for i in range(numInstrucc):
-                    yield env.timeout(velocidadProc)
-                    if (instruccPorRealizar <= 1):
-                        break
-                    else:
-                        instruccPorRealizar = instruccPorRealizar - 1
-                print ('%s ya ha salido del procesador' % nombre)
-        if (instruccPorRealizar > 1):
-            num = random.randint(1,2)
+    
+    inicio = env.now
+    print ('%s ya esta ready' % nombre)
+    with CPU.request() as turno:
+        yield turno
+        print ('%s ya entro al procesador' % nombre)
+        while(instruccPorRealizar >= numInstrucc):
+            yield env.timeout(velocidadProc)
+            instruccPorRealizar = instruccPorRealizar - numInstrucc
+            if (instruccPorRealizar > 1):
+                num = random.randint(1,2)
+            else:
+                break
             if (num == 1):
                 print ('%s entro a waiting' % nombre)
-                yield env.timeout(1)
-    
-    print ('%s Terminated' % nombre)
-    print ('memoria RAM: %d' %memoriaActual)
-    yield capacidadMemoria.put(memoriaActual)
+                yield env.timeout(velocidadProc)
+            print ('%s ya esta ready' % nombre)
+        print ('%s ya ha salido del procesador' % nombre)
+            
+        tiempoTotal = env.now - inicio
+        totalDia = totalDia + tiempoTotal
+        print inicio
+        print ('%s Terminated' % nombre)
+        print ('memoria RAM: %d' %memoriaActual)
+        yield capacidadMemoria.put(memoriaActual)
     
 env = simpy.Environment()
 CPU = simpy.Resource(env, capacity = 1)
-capacidadMemoria = simpy.Container(env, 100, init=10)
+capacidadMemoria = simpy.Container(env, 200, init=10)
 random.seed(10)
-
-for i in range (10):
+ 
+totalDia = 0
+for i in range (200):
     env.process(esteProceso('Proceso %d' % i, env, CPU , capacidadMemoria))
+
 env.run()
 
-print time.clock()-t0
+print ("Tiempo promedio es: "), totalDia/200
